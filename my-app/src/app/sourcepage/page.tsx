29e26498b6dbfaa4a24ed91b3818146eeff21420
Margarita Bershtainis <margo.bershtainis@gmail.com>
2024-05-16 21:16:09 +0300

    testsource

---
+++ my-app/src/app/sourcepage/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+      <main>
+        <p><a href="./sourcepage/source1">SOURCE PAGE</a></p>
+      </main>
+    );
+  }
---
+++ my-app/src/app/sourcepage/source1/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+      <main>
+        <p>SOURCE 1</p>
+      </main>
+    );
+  }
